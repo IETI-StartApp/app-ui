6ad6e4528a6ae1ff2ce269c0f05ef21b7190039f
AlejandroBohal <sergio.bohorquez@mail.escuelaing.edu.co>
2021-03-03 01:37:24 -0500

    ProgressBarTest

--- src/Tests/Components/ProgressBar/ProgressBar.test.js
+++ src/Tests/Components/ProgressBar/ProgressBar.test.js
@@ -1,14 +1,14 @@
 import '@testing-library/jest-dom';
 import { shallow } from 'enzyme';
 import React from 'react';
-import {ProgressBar} from '../../../components/ProgressBar/ProgressBar'
+import {ProgressBar} from '../../../components/Progressbar/ProgressBar'
 
 
 describe('Testing progress bar', () => {
 
     const wrapper = shallow(<ProgressBar activeStep={0}/>)
 
-    test('should load progressbar', () => {
+    test('should load progressbar ', () => {
         expect(wrapper).toMatchSnapshot();
     })
     
