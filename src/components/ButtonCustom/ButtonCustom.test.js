0cc3b4eda00082f0ed8e1fbda54d125cda14868b
Santiago Rubiano <43104565+srubianof@users.noreply.github.com>
2021-03-02 20:51:48 -0500

    feat(component-progress): add CustomButton

---
+++ src/components/ButtonCustom/ButtonCustom.test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {render, screen} from '@testing-library/react';
+import {Button} from "./ButtonCustom";
+
+
+test('renders learn react link', () => {
+    render(<Button onClick={() => console.log("HOLA")}>
+        Start Investing
+    </Button>);
+    const linkElement = screen.getByText(/Start Investing/i);
+    expect(linkElement).toBeInTheDocument();
+});
