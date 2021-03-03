076bfe500419a38ad526f9d5655d858f17bea6cc
Santiago Rubiano <43104565+srubianof@users.noreply.github.com>
2021-03-02 21:10:11 -0500

    test 2

--- src/components/ButtonCustom/ButtonCustom.test.js
+++ src/components/ButtonCustom/ButtonCustom.test.js
@@ -1,11 +1,20 @@
 import React from "react";
 import {render, screen} from '@testing-library/react';
-import {Button} from "./ButtonCustom";
+import {Button, CustomTypography} from "./ButtonCustom";
+import {Wrapper} from "./styles";
 
 test('renders learn react link', () => {
     render(<Button onClick={() => console.log("HOLA")}>
         Start Investing
     </Button>);
     const linkElement = screen.getByText(/Start Investing/i);
+    expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+    render( <CustomTypography text={'red'}>
+        prueba
+    </CustomTypography>);
+    const linkElement = screen.getByText(/prueba/i);
     expect(linkElement).toBeInTheDocument();
 });
