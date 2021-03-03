43bf8a21e3d47e1b05168c906d4645b36811fb60
Angi Jimenez <angi9236@gmail.com>
2021-03-03 17:49:07 -0500

    Project Items (#19)

---
+++ src/components/ProjectItems/styles.js
@@ -0,0 +1,19 @@
+import { makeStyles } from '@material-ui/core';
+
+export const useStyles = makeStyles((theme) => ({
+
+    sizeCard : {
+        maxWidth : '350px',
+        minWidth: '320px',
+        height: '500px',
+    },
+
+    mainColor :{
+        color: '#950740',
+    },
+
+    avatar :{
+        backgroundColor: '#950740',
+    },
+
+}));
