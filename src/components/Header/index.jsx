87b88fad09099b80ff30974db5446720b897641c
Santiago Rubiano <43104565+srubianof@users.noreply.github.com>
2021-04-06 23:03:27 -0400

    Landing page & redirect to login

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
+import {useHistory} from "react-router";
+import logo from "./startapp.png";
+
+export const Header = () => {
+    const history = useHistory();
+
+    return (
+        <>
+            <Toolbar style={{
+                background: 'black',
+                marginBottom: '10px',
+                alignContent: 'center'
+            }}>
+                <div style={{width: '89vw', paddingLeft: '48%'}}>
+                    <img style={{height: '50px', color: 'white'}} src={logo} alt={''}/>
+                </div>
+                <IconButton>
+                    <PersonOutlineIcon style={{
+                        color: 'white'
+                    }}/>
+                </IconButton>
+                <Button style={{
+                    background: "#950740",
+                    color: 'white',
+                    padding: '10px 20px',
+                    width: '106px',
+                    height: '50px'
+                }} size="small" onClick={() => history.push('/signin')}>
+                    Sign in
+                </Button>
+            </Toolbar>
+        </>
+    );
+}
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,3 +3,4 @@ export * from './RegisterPage/RegisterForm';
 export * from './ProjectDescription/ProjectDescription';
 export * from './RegisterProjectPage';
 export * from './UserTypePage/UserSelect';
+export * from './LandingPage'
