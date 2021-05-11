36b59e44e5c731fa5b4b4d626043e3e5ac1b1e3c
FelipeRojas15 <53972640+FelipeRojas15@users.noreply.github.com>
2021-05-11 17:40:37 -0500

    ChatBot component (#35)
    * ChatBot component

---
+++ src/components/ChatBot/styleChatBot.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const ButtonDesign = styled.button`
+width: 100%;
+height: 50px;
+margin-top: 1px;
+border-radius: 2px;
+background: linear-gradient(90deg,
+rgb(149, 7, 64) 0%,
+rgb(127, 6, 54) 100%);
+outline: none;
+border: none;
+align-content: center;
+color: #fff;
+font-size: 1rem;
+
+`;
