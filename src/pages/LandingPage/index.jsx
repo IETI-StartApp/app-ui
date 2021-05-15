7f57df239648c97e87bd110d74854663f7d9a2ba
Santiago Rubiano <43104565+srubianof@users.noreply.github.com>
2021-05-14 21:58:12 -0400

    Update index.jsx

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -1,6 +1,12 @@
 import React, {useEffect, useState} from 'react'
 import JsonData from '../../data/data.json'
+import SmoothScroll from 'smooth-scroll'
 import {About, Features, HeaderT, Navigation, Team} from "../../components";
+
+export const scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 1000,
+    speedAsDuration: true,
+})
 
 export const LandingPage = () => {
     const [landingPageData, setLandingPageData] = useState({})
