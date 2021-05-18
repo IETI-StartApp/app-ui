5eaa4ea9889e5fa109f0c60a8b2e7d0adf036051
AlejandroBohal <sergio.bohorquez@mail.escuelaing.edu.co>
2021-05-17 19:56:52 -0500

    Solved useLocation test

--- src/Tests/Pages/Dashboard/index.test.js
+++ src/Tests/Pages/Dashboard/index.test.js
@@ -3,6 +3,15 @@ import { shallow } from 'enzyme';
 import React from 'react';
 import { Dashboard } from '../../../pages/Dashboard/index';
 
+jest.mock('react-router-dom', () => ({
+    useLocation: jest.fn().mockReturnValue({
+      pathname: '/dashboard',
+      search: '',
+      hash: '',
+      state: {role: "ENTREPRENEUR"},
+      key: '5nvxpbdafa',
+    }),
+}));
 describe('Testing dashboard', () => {
     const wrapper = shallow(<Dashboard />);
                                         
--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -3,7 +3,8 @@ import { InvestorDashboard, EntrepreneurDashboard} from "../../components";
 import { useLocation } from "react-router-dom";
 
 export const Dashboard = () => {
-    const {state : {role} = "ENTREPENEUR"} = useLocation() || {};
+    
+    const {state : {role} = "ENTREPENEUR"} = useLocation();
     return (
         <>
             {
