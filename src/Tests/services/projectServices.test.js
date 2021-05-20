f623be5d90c5935f3e91807723716d89dde7d0b3
Santiago Rubiano <43104565+srubianof@users.noreply.github.com>
2021-05-20 02:31:35 -0400

    fix 50.0001

--- src/Tests/services/projectServices.test.js
+++ src/Tests/services/projectServices.test.js
@@ -1,34 +1,38 @@
-import {postProject} from "../../services/userServices";
+import {postProject} from "../../services/projectServices";
 
-const project = {
-    "name": "WTFWTF",
-    "country": "CO",
-    "description": "HOLASASDFASDFASDFasdfasdf",
-    "finance": {
-        "value": "10000000",
-        "investorNumber": 2,
-        "valuation": "10000000",
-        "minimumInvestment": "1000000",
-        "startDate": "2021-05-20T05:35:30.125Z",
-        "endDate": "2021-05-20T05:35:05.866Z"
-    },
-    "comments": null,
-    "user": {
-        "firstName": "S",
-        "lastName": "a",
-        "email": "srubianof@gmail.com",
-        "identification": 1019151562,
-        "role": "ENTREPRENEUR",
-        "description": "esto es una prueba",
-        "projects": []
+describe('Test user services', () => {
+    beforeAll(() => jest.spyOn(window, 'fetch'))
+
+    const project = {
+        "name": "WTFWTF",
+        "country": "CO",
+        "description": "HOLASASDFASDFASDFasdfasdf",
+        "finance": {
+            "value": "10000000",
+            "investorNumber": 2,
+            "valuation": "10000000",
+            "minimumInvestment": "1000000",
+            "startDate": "2021-05-20T05:35:30.125Z",
+            "endDate": "2021-05-20T05:35:05.866Z"
+        },
+        "comments": null,
+        "user": {
+            "firstName": "S",
+            "lastName": "a",
+            "email": "srubianof@gmail.com",
+            "identification": 1019151562,
+            "role": "ENTREPRENEUR",
+            "description": "esto es una prueba",
+            "projects": []
+        }
     }
-}
-test('should post', async () => {
-    window.fetch.mockResolvedValueOnce({
-        ok: true,
-        status: 201,
-        json: async () => ({}),
-    })
-    const user = await postProject(project);
-    expect(user).toBe(undefined);
-});
+    test('should post', async () => {
+        window.fetch.mockResolvedValueOnce({
+            ok: true,
+            status: 201,
+            json: async () => ({}),
+        })
+        const user = await postProject(project);
+        expect(user).toBe(undefined);
+    });
+})
