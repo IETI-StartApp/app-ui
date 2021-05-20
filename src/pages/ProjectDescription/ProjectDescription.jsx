b44746cb6670fee709a34a1dd203efa5a82f66a0
Santiago Rubiano <43104565+srubianof@users.noreply.github.com>
2021-05-19 22:17:47 -0400

    test

--- src/pages/ProjectDescription/ProjectDescription.jsx
+++ src/pages/ProjectDescription/ProjectDescription.jsx
@@ -11,11 +11,8 @@ export const ProjectDescription = (props) => {
     const formatter = new Intl.NumberFormat('en-US', {
         style: 'currency',
         currency: 'USD',
-
-        // These options are needed to round to whole numbers if that's what you want.
-        //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
-        //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
     });
+    console.log(user)
     const {height, width} = useWindowDimensions() || {};
     return (
         <>
@@ -36,8 +33,8 @@ export const ProjectDescription = (props) => {
                             valoracion={formatter.format(finance.valuation)}
                             InversionMinima={formatter.format(finance.minimumInvestment)}
                             socios={finance.investorNumber}
-                            fechaInicio={finance.startDate.substring(0, 10)}
-                            FechaFin={finance.endDate.substring(0, 10)}/>
+                            fechaInicio={finance.startDate}
+                            FechaFin={finance.endDate}/>
                     </Grid>
                 </Grid>
                 <Comment/>
